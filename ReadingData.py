8cb0f06af3c6094bd0c46c21b658ea96c824af2c
divyamagwl22 <113692349+divyamagwl22@users.noreply.github.com>
2023-04-26 09:54:44 +0200

    Update ReadingData.py

--- ReadingData.py
+++ ReadingData.py
@@ -1,3 +1,13 @@
-import panda as pd 
-morning1 = pd.read_excel('Data1.xlsb', sheet+name=DATA 1)
-print(morning1)
+import pandas as pd 
+# DATA 1
+Thermocouples1 = pd.read_excel('Data 1.xlsb', engine='pyxlsb', sheet_name= 'Thermocouples')
+Pressure1 = pd.read_excel('Data 1.xlsb', engine='pyxlsb', sheet_name= 'Pressure Sensors')
+Load1 = pd.read_excel('Data 1.xlsb', engine='pyxlsb', sheet_name= 'Load Cells')
+
+# DATA 2
+Thermocouples2 = pd.read_excel('Data 2.xlsx', sheet_name= 'Thermocouples')
+Pressure2 = pd.read_excel('Data 2.xlsx', sheet_name= 'Pressure Sensors')
+Load2 = pd.read_excel('Data 2.xlsx', sheet_name= 'Load Cells')
+
+print(Load2)
+
