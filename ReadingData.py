fe5707a56a4503f7ced08077d37d13a389721772
divyamagwl22 <113692349+divyamagwl22@users.noreply.github.com>
2023-04-26 10:51:16 +0200

    Update ReadingData.py

--- ReadingData.py
+++ ReadingData.py
@@ -1,18 +1,15 @@
 import pandas as pd 
 # DATA 1
-Thermocouples1 = pd.read_excel('Data 1.xlsb', engine='pyxlsb', sheet_name= 'Thermocouples')
-Thermocouples1.to_csv('Thermocouples1', index=False)
-Pressure1 = pd.read_excel('Data 1.xlsb', engine='pyxlsb', sheet_name= 'Pressure Sensors')
-Pressure1.to_csv('Pressure1', index=False)
-Load1 = pd.read_excel('Data 1.xlsb', engine='pyxlsb', sheet_name= 'Load Cells')
-Load1.to_csv('Load1', index=False)
+Thermocouples1 = pd.read_csv('Thermocouples1.csv')
+Pressure1 = pd.read_csv('Pressure1.csv')
+Load1 = pd.read_csv('Load1.csv')
+
 
 # DATA 2
-Thermocouples2 = pd.read_excel('Data 2.xlsx', sheet_name= 'Thermocouples')
-Thermocouples2.to_csv('Thermocouples2', index=False)
-Pressure2 = pd.read_excel('Data 2.xlsx', sheet_name= 'Pressure Sensors')
-Pressure2.to_csv('Pressure2', index=False)
-Load2 = pd.read_excel('Data 2.xlsx', sheet_name= 'Load Cells')
-Load2.to_csv('Load2', index=False)
+Thermocouples2 = pd.read_csv('Thermocouples2.csv')
+Pressure2 = pd.read_csv('Pressure2.csv')
+Load2 = pd.read_csv('Load2.csv')
+
+print(Load2[:,0])
 
 
