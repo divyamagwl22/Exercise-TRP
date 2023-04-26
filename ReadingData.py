b76da091df4a30e3cc977de2da95d23cca56433e
divyamagwl22 <113692349+divyamagwl22@users.noreply.github.com>
2023-04-26 11:11:01 +0200

    Update ReadingData.py

--- ReadingData.py
+++ ReadingData.py
@@ -1,12 +1,13 @@
 import pandas as pd 
 # DATA 1
+# Read CSV file for Thermocouple data from Datasheet 1
 Thermocouples1 = pd.read_csv('Thermocouples1.csv')
+# Convert data to numpy accessable
 T1 = Thermocouples1.to_numpy()
 Pressure1 = pd.read_csv('Pressure1.csv')
 P1 = Pressure1.to_numpy()
 Load1 = pd.read_csv('Load1.csv')
 L1 = Load1.to_numpy()
-
 
 # DATA 2
 Thermocouples2 = pd.read_csv('Thermocouples2.csv')
@@ -16,6 +17,7 @@ P2 = Pressure2.to_numpy()
 Load2 = pd.read_csv('Load2.csv')
 L2 = Load2.to_numpy()
 
+# Example: Index out 3rd column from Thermocouple sheet
 print(T2[:,3])
 
 
