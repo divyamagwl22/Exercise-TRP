573608029757b561dcb0f90a556fbdd1ee955b64
divyamagwl22 <113692349+divyamagwl22@users.noreply.github.com>
2023-04-26 11:00:28 +0200

    Update ReadingData.py

--- ReadingData.py
+++ ReadingData.py
@@ -1,15 +1,21 @@
 import pandas as pd 
 # DATA 1
 Thermocouples1 = pd.read_csv('Thermocouples1.csv')
+T1 = Thermocouples1.to_numpy()
 Pressure1 = pd.read_csv('Pressure1.csv')
+P1 = Pressure1.to_numpy()
 Load1 = pd.read_csv('Load1.csv')
+L1 = Load1.to_numpy()
 
 
 # DATA 2
 Thermocouples2 = pd.read_csv('Thermocouples2.csv')
+T2 = Thermocouples2.to_numpy()
 Pressure2 = pd.read_csv('Pressure2.csv')
+P2 = Pressure2.to_numpy()
 Load2 = pd.read_csv('Load2.csv')
+L2 = Load2.to_numpy()
 
-print(Load2[:,0])
+print(T2[:,3])
 
 
